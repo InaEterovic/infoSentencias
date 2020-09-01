331d27a7e0da7a6a9b3652871c7e77e95ff5e141
Irina Eterovic <inaeterovic@gmail.com>
2020-09-01 16:06:16 -0400

    implemented main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router'
 import { EstadisticasGeneralesComponent } from './components/estadisticas-generales/estadisticas-generales.component';
 import { EstadisticasDelitoComponent } from './components/estadisticas-delito/estadisticas-delito.component';
-
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+    {path:'', component:HomeComponent},
     {path:'estadisticas-generales', component:EstadisticasGeneralesComponent },
     {path:'estadisticas-delito', component: EstadisticasDelitoComponent} 
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports : [RouterModule]
+
+exports : [RouterModule]
 })
 export class AppRoutingModule{}

2c500e827853f885011c504344277fd966266a35
Irina Eterovic <inaeterovic@gmail.com>
2020-10-20 10:11:50 -0400

    Implemented structre for general statistics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+
 import {AppRoutingModule} from './app-routing.module';
+import { environment } from './../environments/environment';
 import { EstadisticasGeneralesComponent } from './components/estadisticas-generales/estadisticas-generales.component';
 import { EstadisticasDelitoComponent } from './components/estadisticas-delito/estadisticas-delito.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -22,10 +28,13 @@ import { EstadisticasDelitoComponent } from './components/estadisticas-delito/es
     
   ],
   imports: [
-  
-  AppRoutingModule,
+    CommonModule,
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+ 
+  firebaseConfig: {
+    apiKey: "AIzaSyC22YiKFUGzXTBMflIT4Lzt7AMd80-y8V4",
+    authDomain: "informacion-sentecias.firebaseapp.com",
+    databaseURL: "https://informacion-sentecias.firebaseio.com",
+    projectId: "informacion-sentecias",
+    storageBucket: "informacion-sentecias.appspot.com",
+    messagingSenderId: "222026685890",
+    appId: "1:222026685890:web:18f5177b465e604634ca5b",
+    measurementId: "G-P2S107D6CV"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
